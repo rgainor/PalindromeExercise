3866594f75f7e2a3a8902fa9eaebff45603c6a45
Ryan Gainor <rmgainor@gmail.com>
2023-06-05 23:21:39 -0500

    exercise completed

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+			var reversed = "";
+
+			for(int i = word.Length - 1; i >= 0; i--)
+			{
+				reversed += word[i];
+			}
+			
+			//var wordCharArr = word.ToCharArray();
+			//Array.Reverse(wordCharArr);
+			//var reversed = new string(wordCharArr);
+
+			if(word.ToLower() == reversed.ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
